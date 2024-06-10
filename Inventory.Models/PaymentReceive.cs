ecc94a3aeecea1b443cead81b50cd8943f95f54e
Md.Ishak Jewel <ishakjewel@gmail.com>
2024-06-10 23:46:02 +0600

    Update

---
+++ Inventory.Models/PaymentReceive.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inventory.Models
+{
+    public class PaymentReceive
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int InvoiceId { get; set; }
+        public DateTimeOffset DateOfPayment { get; set; }
+        public int PaymentTypeId { get; set; }
+        public double PaymentAmount  { get; set; }
+
+    }
+}
